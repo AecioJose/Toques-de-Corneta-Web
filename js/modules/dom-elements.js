652bfd646c9f0a63e6e8c4f4763d251ceaa6bbe0
Icaro <146675789+IcaroRubem@users.noreply.github.com>
2024-08-29 22:22:30 +0000

    UPLOAD: upload files of test (PFL Monitor)

--- js/modules/dom-elements.js
+++
@@ -1,5 +0,0 @@
-export const buttonContainer = document.getElementById('button-container');
-export const addTonesBtn = document.getElementById('add-tones-btn');
-export const editBtnNchangeOrder = document.getElementById('edit-btn');
-export const changeOrderImg = document.getElementById('edit-img');
-
