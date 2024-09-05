b129251b6790a6522f6699777f8bfd8dd041b492
Icaro <146675789+IcaroRubem@users.noreply.github.com>
2024-09-05 20:22:48 +0000

    UPDATE: separate script of html

--- script.js
+++ script.js
@@ -1,148 +1,127 @@
-class AudioControler {
-    constructor() {
-        this.audioElement = new Audio()
-        this.audioCtx = null
-        this.track = null
-        this.gainNode = null
-        this.panner = null
-        this.analyser = null
-        this.bufferLength = null
-        this.dataArray = null
-        this.splitter = null
-        this.leftChannel = null
-        this.rightChannel = null
-        this.leftDataArray = null
-        this.rightDataArray = null
-        this.vol = {
-            min: 0,
-            max: 2,
-            default: 1
-            }
-            this.pan = {
-                min: -1,
-                max: 1,
-            default: 0
-            }
-            this.initialized = false
-            //step = 0.01
-            //-1 left / 1 right
-        }
+console.clear();
 
-        initialize() {
-            if (!this.initialized) {
-                this.audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-                this.track = new MediaElementAudioSourceNode(this.audioCtx, {
-                    mediaElement: this.audioElement
-                })
-                this.gainNode = new GainNode(this.audioCtx)
-                this.gainNode.gain.value = this.vol.default
-                this.panner = new StereoPannerNode(this.audioCtx, {
-                    pan: this.pan.default
-                })
-                this.analyser = this.audioCtx.createAnalyser()
-                this.analyser.fftSize = 32
-                this.splitter = this.audioCtx.createChannelSplitter(2)
-                this.leftChannel = this.audioCtx.createAnalyser()
-                this.leftChannel.fftSize = 32
-                this.rightChannel = this.audioCtx.createAnalyser()
-                this.rightChannel.fftSize = 32
-                this.bufferLength = this.analyser.frequencyBinCount
-                this.dataArray = new Uint8Array(this.bufferLength)
-                this.leftDataArray = new Uint8Array(this.leftChannel.frequencyBinCount)
-                this.rightDataArray = new Uint8Array(this.rightChannel.frequencyBinCount)
-                this.connect()
-                this.initialized = true
-            }
-        }
 
-        connect() {
-            this.track.connect(this.analyser).connect(this.gainNode).connect(this.panner  ).connect(this.splitter)
-            this.panner.connect(this.audioCtx.destination)
+let audioCtx;
+const audioControler = new AudioControler()
 
-            this.splitter.connect(this.leftChannel, 0)
-            this.splitter.connect(this.rightChannel, 1)
-        }
+const audioElement = document.querySelector("audio");
+const fileElement = document.querySelector("#file");
+const volumeControl = document.querySelector('#volume');
+const pannerControl = document.querySelector('#panner');
 
-        setSource(audioPath) {
-            if (audioPath) {
-                this.audioElement.src = audioPath
-            }
-        }
 
-        audioContextSuspended() {
-            if (this.audioCtx.state === "suspended") {
-                this.audioCtx.resume();
-            }
-        }
+fileElement.addEventListener("change", (e) => {
+    audioControler.initialize()
+    audioControler.setSource(URL.createObjectURL(e.target.files[0]))
+})
 
-        getByteFrequencyData() {
-            if (!this.initialized) return []
-            this.analyser.getByteFrequencyData(this.dataArray)
-            return this.dataArray
-        }
+volumeControl.addEventListener(
+    "input",
+    (event) => {
+        audioControler.setVol(event.target.value)
+    },
+    false
+);
 
-        getByteFrequencyDataR() {
-            if (!this.initialized) return []
-            this.rightChannel.getByteFrequencyData(this.rightDataArray)
-            return this.rightDataArray
-        }
+pannerControl.addEventListener(
+    "input",
+    (event) => {
+        audioControler.setPan(event.target.value)
+    },
+    false
+);
 
-        getByteFrequencyDataL() {
-            if (!this.initialized) return []
-            this.leftChannel.getByteFrequencyData(this.leftDataArray)
-            return this.leftDataArray
-        }
+function playpause() {
+    if (audioControler.audioElement.paused) {
+        audioControler.play()
+    } else {
+        audioControler.pause()
+    }
+}
 
-        getMedia(dataarray) {
-            if (dataarray.length === 0) {
-                return 0
-            }
+const canvas = document.getElementById('audioCanvas');
+const canvasCtx = canvas.getContext('2d');
 
-            const sum = dataarray.reduce((acc, num) => acc + num, 0);
+const analyserCanvas = document.getElementById('analyserCanvas');
+const canvasCtxAnalyser = analyserCanvas.getContext('2d');
 
-            return sum / dataarray.length;
-        }
+const lineOld = { l: canvas.height, r: canvas.height }
+const lineh = 2
+const dec = 0.7
 
-        map(valor, faixaMinOriginal, faixaMaxOriginal, faixaMinNova, faixaMaxNova) {
-            
-            return faixaMinNova + ((valor - faixaMinOriginal) / (faixaMaxOriginal - faixaMinOriginal)) * (faixaMaxNova - faixaMinNova);
-        }
+function draw() {
+    canvasCtx.fillStyle = '#000000';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+    const analyserData = audioControler.getByteFrequencyData()
+    const dataL = audioControler.getByteFrequencyDataL()
+    const dataR = audioControler.getByteFrequencyDataR()
 
-        play() {
-            if (!this.initialized) return
+    let L = audioControler.getMedia(dataL)
+    let R = audioControler.getMedia(dataR)
 
-            this.audioContextSuspended()
-            if (this.audioElement.paused) {
-                this.audioElement.play()
-            }
-        }
+    let Lh = audioControler.map(R, 0, 255, 0, canvas.height)
+    let Rh = audioControler.map(L, 0, 255, 0, canvas.height)
 
-        pause() {
-            if (!this.initialized) return
+    canvasCtx.fillStyle = "#ffffff"
+    canvasCtx.fillRect(
+        90,
+        lineOld.l - lineh,
+        30,
+        lineh)
+    let grad = canvasCtx.createLinearGradient(90, canvas.height - Lh, 30, canvas.height)
+    grad.addColorStop(0, `hsl(${audioControler.map(L, 0, 255, 130, 0)}, 100%, 50%)`)
+    grad.addColorStop(1, "hsl(120,100%,50%)")
+    canvasCtx.fillStyle = grad
+    canvasCtx.fillRect(
+        90,
+        canvas.height - Lh,
+        30,
+        canvas.height)
+    if (canvas.height - Lh < lineOld.l) {
+        lineOld.l = canvas.height - Lh
+    }
 
-            this.audioContextSuspended()
-            if (!this.audioElement.paused) {
-                this.audioElement.pause()
-            }
-        }
+    canvasCtx.fillStyle = "#ffffff"
+    canvasCtx.fillRect(
+        30,
+        lineOld.r - lineh,
+        30,
+        lineh)
+    canvasCtx.fillStyle = "rgb(49,255,9)"
+    canvasCtx.fillRect(
+        30,
+        canvas.height - Rh,
+        30,
+        canvas.height)
+    if (canvas.height - Rh < lineOld.r) {
+        lineOld.r = canvas.height - Rh
+    }
 
-        setVol(volValue) {
-            if (volValue && this.initialized) {
-                if (volValue > this.vol.max || volValue < this.vol.min) {
-                    return
-                }
 
-                this.gainNode.gain.value = volValue
-            }
-        }
-
-        setPan(panValue) {
-            if (panValue && this.initialized) {
-                if (panValue < this.pan.min || panValue > this.pan.max) {
-                    return
-                }
-
-                this.panner.pan.value = panValue
-            }
+    if (lineOld.l < canvas.height) {
+        if ((canvas.height - Lh) - lineOld.l > 40 || canvas.height - Lh == 0) {
+            lineOld.l += dec * 3
+        } else {
+            lineOld.l += dec
         }
     }
+    if (lineOld.r < canvas.height) {//lineOld.r += dec
+        if ((canvas.height - Rh) - lineOld.r > 40 || canvas.height - Rh == 0) {
+            lineOld.r += dec * 3
+        } else {
+            lineOld.r += dec
+        }
+    }
+    /* SECOND CANVAS */
+    canvasCtxAnalyser.clearRect(0, 0, analyserCanvas.width, analyserCanvas.height);
+    canvasCtxAnalyser.fillStyle = '#ffffff';
+    for (let i = 0; i < analyserData.length; i++) {
+        let barw = analyserCanvas.width / analyserData.length
+        let barh = audioControler.map(analyserData[i], 0, 255, 3, analyserCanvas.height)
+        canvasCtxAnalyser.fillRect(barw * i, analyserCanvas.height - barh, barw, barh)
+    }
+    /* ============== */
+    requestAnimationFrame(draw);
+}
+
+draw()
